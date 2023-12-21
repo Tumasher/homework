dd223d525ace1631e588bb26cffd3e56290d8251
Tumasher <150005181+Tumasher@users.noreply.github.com>
2023-12-21 18:54:23 +0300

    Add files via upload

---
+++ hw_task_6/task_1.py
@@ -0,0 +1,17 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+
+data = np.loadtxt('data.txt', delimiter=',')
+X = data[:, :-1]
+y = data[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = Sequential()
+model.add(Dense(12, input_dim=X_train.shape[1], activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=15, batch_size=10, verbose=2)
+predictions = model.predict(X_test[:3])
+print("Predictions:", predictions)
